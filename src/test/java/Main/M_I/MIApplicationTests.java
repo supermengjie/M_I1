20ff6e10106b64865ba13becaeb55a969de54d2e
supermengjie <yem@gmail.com>
2020-01-23 22:24:33 -0500

    initial commit

---
+++ src/test/java/Main/M_I/MIApplicationTests.java
@@ -0,0 +1,16 @@
+package Main.M_I;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MIApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
